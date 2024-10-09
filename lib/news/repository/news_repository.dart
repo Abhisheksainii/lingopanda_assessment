c8aeccdf92d7468575fb215d8abf7468e20ca9c1
Abhisheksainii <abhisheksaini1219@gmail.com>
2024-10-09 15:41:58 +0530

    feat(common): setup

---
+++ lib/news/repository/news_repository.dart
@@ -0,0 +1,11 @@
+class NewsRepository {
+  NewsRepository._();
+
+  static Future<void> fetchTopHeadlines({
+    required String countryCode,
+  }) async {
+    try {} catch (e) {
+      print("e");
+    }
+  }
+}
